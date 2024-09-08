05bda0198d7d53253009479939e274995d0c193f
tsukiram <weebdur@gmail.com>
2024-09-08 22:39:10 +0700

    init

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rama/flask/ram-ai/backend/venv"
+set -gx VIRTUAL_ENV "/home/rama/flask/ram-ai/rama/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
